92cdbde992fd223cdb5e132173f5ab4436e4bc51
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-08-26 21:54:06 +0200

    Nest characters routes inside teams

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root 'team#index'
 
-  resources :characters
-  resources :teams
+  resources :teams do
+    resources :characters
+  end
   devise_for :users
 end
