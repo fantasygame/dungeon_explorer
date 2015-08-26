5ac47fe89995f092d09add307602362cbbb4c70b
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-08-26 22:05:20 +0200

    Reverse commit Nest characters routes inside teams

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   root 'team#index'
 
-  resources :teams do
-    resources :characters
-  end
+  resources :characters
+  resources :teams
   devise_for :users
 end
