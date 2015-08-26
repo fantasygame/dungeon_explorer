a704103216f5a2903ada2771775d6912b36a4590
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-08-26 21:51:12 +0200

    Add validations and dependencies to team and character

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ActiveRecord::Base
   belongs_to :team
+  validates :name, presence: :true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
+  has_many :characters, dependent: :destroy
+  validates :name, presence: :true
 
   def to_s
     name
