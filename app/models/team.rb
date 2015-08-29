5d2baaf4574967ab5cde0a4bf9f3048708d4b9dd
Daktrian <pabielicki@hotmail.com>
2015-08-29 18:59:48 +0200

    Added level method for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,21 @@ class Team < ActiveRecord::Base
   def to_s
     name
   end
+
+  def level
+  return 1 if characters.empty?
+    else avglevel
+
+  end
+end
+
+
+private
+
+def avglevel
+  teamlevel = []
+  characters.each do |character|
+    teamlevel << character.level
+  end
+  return (teamlevel.inject(0.00){ |sum, el| sum + el } / teamlevel.size).round
 end
