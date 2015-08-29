723ea88e368ca4452690e550c681c0de4d362c43
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-08-29 21:21:21 +0200

    Monster decorator

---
+++ app/decorators/monster_decorator.rb
@@ -0,0 +1,9 @@
+class MonsterDecorator < Draper::Decorator
+  delegate_all
+
+  def name_pluralize(number)
+    name = object.name.split(',')
+    name[0] = name[0].pluralize if number > 1
+    name.join(', ')
+  end
+end
