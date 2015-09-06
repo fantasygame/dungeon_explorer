c2afa1d0be66f6be36ef04d9658693053233272e
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-09-06 13:29:19 +0200

    Add pagination to items index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   expose(:item, attributes: :team_params)
   expose(:team)
-  expose(:items) { team.items }
+  expose(:items) { team.items.page(params[:page]) }
   expose(:item)
 end
